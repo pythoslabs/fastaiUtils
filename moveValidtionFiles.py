1494f9030388bcd59ec7a4c09f8025d29f2fd094
Pythos Labs <48703756+pythoslabs@users.noreply.github.com>
2019-08-09 13:22:23 +0530

    Create moveValidtionFiles.py

---
+++ moveValidtionFiles.py
@@ -0,0 +1,20 @@
+#Moves all the images in the validation folder back to their corresponding classes in 
+# train folder 
+def moveValidtionFiles(root_path):
+    root_path= Path(root_path)
+    valid_folder = root_path/'valid'
+    train_folder= root_path/'train'
+    
+    valid_classes = os.listdir(valid_folder)    
+    print(valid_classes)
+    for valid_class in valid_classes :
+        if(os.path.exists(train_folder/valid_class)) :
+            valid_class_files = os.listdir(valid_folder/Path(valid_class))
+            no_of_files_valid = len(valid_class_files)    
+            for file_to_move in valid_class_files:
+                src = valid_folder/Path(valid_class)/file_to_move
+                dest = train_folder/Path(valid_class)/file_to_move        
+                #print("src = %s dest = %s" %(src,dest))
+                shutil.move(src,dest)
+        else :
+            print("ATTENTION - Train folder - %s - does not exist" %(valid_class))     
